e1252aafda45027804f4715f2c6ac7920951b0f2
awiai <meroalwznem@gmail.com>
2026-07-09 04:31:29 +0300

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUMsfZ4bNr2xRQyx50gdhID4oGhILDFrI",
+  authDomain: "market-alameer-15f2d.firebaseapp.com",
+  projectId: "market-alameer-15f2d",
+  storageBucket: "market-alameer-15f2d.firebasestorage.app",
+  messagingSenderId: "981021538410",
+  appId: "1:981021538410:web:1be7ed70e4272391ebf213",
+  measurementId: "G-X1K2NPH0TS"
+};
